477ba4893b52006269bf76893eafe57733fc68ea
em4gon <em4gon@gmail.com>
2017-10-20 19:17:27 -0300

    Update snake_beta.rkt

--- game_project/snake_beta.rkt
+++ game_project/snake_beta.rkt
@@ -1,33 +1,42 @@
 ;(require htdp/gui)
 (require 2htdp/universe)
+;(display-mode normal)
 ;Worlds and the Universe: "universe.rkt"
-(define (inicial c) (/ ALTO 2) )
 (define ALTO 800)
 (define ANCHO 800)
+(define INICIAL (make-posn (/ ANCHO 2) (/ ALTO 2)))
+
 (define FONDO (empty-scene ANCHO ALTO "black")) 
 
-(define (BOTON2 c) (place-image/align (rectangle 100 100 "solid" "blue") 200 200 "middle" "middle" FONDO))
+(define POSBOT1 (make-posn (/ ANCHO 2) (/ ALTO 3) ))
+
+(define (BOTON2 varbot2) (place-image/align (rectangle 100 100 "solid" "blue") 200 200 "middle" "middle" FONDO))
+
+
 ;boton2 options
 ;boton1 play
-(define (BOTON1 c) (place-image/align (rectangle 100 100 "solid" "red") 200 200 "middle" "top" BOTONDOS))
+(define (BOTON1 varbot1) (place-image (rectangle 100 100 "solid" "red") (posn-x POSBOT1) (posn-y POSBOT1) FONDO))
 
 
-(define POSICIONBOTON1 (- ALTO (image-height BOTON
 
-
+#|
 (define (mouse-handler c x y event) 
-  
-  (cond [(string=? event "button-down") 
+    (cond [(string=? event "button-down") 
          (if (and (< y ALTO) 
                   (> y 0))
-             y
+             "white"
              (if (< y POSMAX) POSMAX (if (> y POSMIN) POSMIN y)))]
         [else c]
         )
 )
+|#
 
-(big-bang inicial
- [to-draw BOTONPLAY]
+;funcion auxiliar para cada boton
+;RESULTADOBOTON1: x y -> string (color)
+;(define (RESULTADOBOTON1 x y)
+
+(big-bang INICIAL
+ [to-draw BOTON1]
  ;[on-tick ColorChange 0.5]
  ;[on-key ]
  ;[on-mouse]
